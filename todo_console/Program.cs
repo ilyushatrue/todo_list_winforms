74a3f26e1de29e976f920c279aca4fa675d0b383
ilyushatrue <i.truhin@cmachine.ru>
2024-01-28 11:24:43 +1000

    тестовый консольный проект

---
+++ todo_console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace todo_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
